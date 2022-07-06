fb166ad2a35b98329f5801828889000cef1f3e64
kzhang19 <1349605984@qq.com>
2022-07-06 20:00:40 +0800

    修改config.py

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
 '''
-save the config
+config file:
+参数配置文件
 '''
+import argparse
+import torch 
+from os import path
+
+def get_config():
+    config = argparse.ArgumentParser()
+
+    # load/save data 路径参数
+    config.add_argument('--log_dir',type=str,default='log',)
+
